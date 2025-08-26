feb755635c39f75d1a61ccb5daf284939f771a70
AdelsGray <adelaisabelgarayalvarado@gmail.com>
2025-08-26 17:00:07 -0600

    Integración de ProductManager con funciones de actualizar y eliminar productos

---
+++ investigacion-integracion/mi-ecommerce/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ProductManager from './pages/ProductManager';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <ProductManager />
+    </div>
+  );
+};
+
+export default App;
